4376c7141256a28bec4d6414ba8287529dcf312a
Prime_Midia <elimar.pc@gmail.com>
2021-02-08 14:49:09 -0300

    2º - Adição da imagem da logo e alteração de textos no sistema (Falta a adição do video editado)

--- contact.php
+++ contact.php
@@ -1,12 +1,12 @@
 <?php 
-$myemail = "templates@creative-era.com"; // Change this email ID with your email ID
+$myemail = "monitoramento@protecaoprime.com.br"; // Change this email ID with your email ID
 //$myemail = "inquiry@rapid-coder.com"; // Change this email ID with your email ID
 $email = strip_tags($_POST['email']);
 $formcontent = "<div style='width: 300px;'>";
-$formcontent .= "<div style='background-color:#ff3b30; padding:4px; text-align:center; color:#ffffff;'>TICTOC CONTACT FORM</div>";
+$formcontent .= "<div style='background-color:#ff3b30; padding:4px; text-align:center; color:#ffffff;'>Site Novo Proteção Prime</div>";
 $formcontent .= "<div><p><b>Email:</b> " . $email . "</p></div>";
 $formcontent .= "</div>";
-$subject = "TICTOC contact form";
+$subject = "Site Novo";
 $headers = "From: " . $email . "\r\n";
 $headers .= "Reply-To: ". $email . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
